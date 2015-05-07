328d1b661806d5d33c18ac8d006ff712cc88d485
SRA <saghabozorgi@gmail.com>
2015-05-07 14:45:07 -0400

    complete with notations

--- 1.py
+++ 1.py
@@ -7,7 +7,7 @@ import numpy as np
 
 conn = pypyodbc.connect('DSN=localMSSQL')
 cur = conn.cursor()
-consid='174572'
+consid='70434'
 sql= "select * from saeed_const_transac_interac_states where constituentlookupid='%s'" % consid
 cur.execute(sql)
 x=cur.fetchall();
@@ -21,7 +21,7 @@ cur.close()
 conn.close()
 
 fig = plt.figure()
-
+col=np.array(['r','c','m','g'])
 df['act_int'] = np.nan
 df.loc[df[3] == 'inactive', 'act_int'] = 0
 df.loc[df[3] == 'Donation', 'act_int'] = 1
@@ -63,20 +63,23 @@ groups_pay = dpay.groupby(df2[3])
 #---------------------------------------------------------------------
 
 ## the button axes
-ax2 = fig.add_subplot(3,1,1)
-ax2.set_title('Constituent Transaction-Interaction')
+ax2 = fig.add_subplot(311)
+ax2.set_title('Constituent Transaction-Interaction %s' % consid )
 ax2.set_ylabel('Actions')
 
 
 for name, group in groups_int:
     a=[6]*group[0].count()
     ax2.plot_date(group[1], a, marker='*', linestyle='', ms=7, label=name)
-        
+i=0;        
 for name, group in groups_gif:
-    ax2.plot_date(group[1], group['act_int'], marker='o', linestyle='', ms=7, label=name)
-    
+    ax2.plot_date(group[1], group['act_int'], marker='o', linestyle='', ms=7, label=name, color=col[i])
+    i=i+1
+i=1;
 for name, group in groups_pay:
-    ax2.plot_date(group[1], group['act_int'], marker='o', linestyle='', ms=3, label=name,markeredgecolor=None)        
+    rects=ax2.plot_date(group[1], group['act_int'], marker='o', linestyle='', ms=3, label=name,markeredgecolor=None) 
+    i=i+1  
+   
 ax2.legend(loc=3)
 
 ax2.set_yticks((0,1,2,3,4,6))
@@ -88,34 +91,47 @@ labels = ax2.set_yticklabels(('inactive','donation', 'rec gift', 'pledge', 'plan
 #      arrowprops=dict(arrowstyle="->", connectionstyle="angle,angleA=0,angleB=90,rad=10"),)
 ax2.set_ylim([-1,7])
 ax2.legend(loc='upper center', bbox_to_anchor=(0.5, -0.05), fancybox=True, shadow=True, ncol=5)
-ax2.autoscale_view()
+#ax2.autoscale_view()
 ax2.grid(True)
+
+
+
+
+
+
+
+
 #---------------------------------------------------------------------
-ax4 = fig.add_subplot(3,1,2)
-ax4.set_ylabel('Payment')
+ax4 = fig.add_subplot(312, sharex=ax2)
+ax4.set_ylabel('Payment') 
 groups_payment = df2.groupby(df2[3])
 i=0;
-col=np.array(['r','c','m'])
+
 for name, group in groups_payment:
     #ax4.plot_date(group[1], group[4],  linestyle='-', ms=2, label=name)
-    ax4.bar(group[1], group[4], label=name,width=25, color=col[i], alpha=.5, edgecolor = None)
+    rects=ax4.bar(group[1], group[4], label=name,width=25, color=col[i], alpha=.5, edgecolor = None)
     i=i+1
+    # attach some text labels
+#     for rect in rects:
+#         height = rect.get_height()
+#         ax4.text(rect.get_x(), 1.05*float(height), '%dk'%int(height/1000), ha='center', va='bottom')  
 ax4.xaxis_date()
 ax4.legend(loc='upper center', bbox_to_anchor=(0.5, -0.05), fancybox=True, shadow=True, ncol=5)
-ax4.autoscale_view()
+#ax4.autoscale_view()
 ax4.grid(True)
 #---------------------------------------------------------------------
 
-ax3 = fig.add_subplot(3,1,3)
+ax3 = fig.add_subplot(313,sharex=ax2)
 ax3.set_ylabel('Accumulative revenue')
 df2['acum_amount'] = np.cumsum(df2[4])
 ax3.plot_date(df2[1], df2['acum_amount'],  linestyle='-', marker='')
 
+i=0
 for name, group in groups_gif:
-    ax3.plot_date(group[1], group[4], marker='o', linestyle='', ms=7, label=name)
-
+    ax3.plot_date(group[1], group[4], marker='o', linestyle='', ms=7, color=col[i],label=name)
+    i=i+1
 ax3.legend(loc='upper center', bbox_to_anchor=(0.5, -0.05), fancybox=True, shadow=True, ncol=5)
-ax3.autoscale_view()
+#ax3.autoscale_view()
 ax3.grid(True)
 
 fig.subplots_adjust(hspace=.5)
